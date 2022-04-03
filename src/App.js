f31a3081ffce1995beeebe057f1cd9e3f2bf0dee
unknown <tamiloreolaniyi@gmail.com>
2022-04-03 19:56:33 +0100

    components 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import React from 'react';
 import './App.css';
+import{ BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from './components/pages/Home';
+import NavBar from './components/NavBar';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+    <>
+    <Router>
+        <NavBar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' exact component={Services} />
+          <Route path='/products' exact component={Products} />
+          <Route path='/sign-up' exact component={SignUp} />
+        </Switch>
+      </Router>
+    </>
+    );
 }
 
 export default App;
